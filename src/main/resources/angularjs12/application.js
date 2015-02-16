3fb275d348caf8631f8584de57fb937c5a716af4
Felix Amerbauer <felix@amerbauer.at>
2015-02-16 02:40:07 +0100

    wip

--- src/main/resources/angularjs12/application.js
+++ src/main/resources/angularjs12/application.js
@@ -1,20 +1,29 @@
-angular.module('ui.bootstrap.demo', [ 'ui.bootstrap' ]);
-angular.module('ui.bootstrap.demo').controller('ButtonsCtrl',
-		function($scope, $http) {
-			$scope.singleModel = 1;
+angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('MainCtrl',
+    function ($scope, $http) {
+        function loadData() {
+            $http.get('http://127.0.0.1:8080/service').success(function (data) {
+                console.log("load data: " + JSON.stringify(data));
+                json = angular.fromJson(data);
+                $scope.data = json;
+            });
+        };
+        loadData();
 
-			$scope.radioModel = 'Middle';
-
-			$scope.checkModel = {
-				left : false,
-				middle : true,
-				right : false
-			};
-
-			$scope.$watch('singleModel', function() {
-				$http.post('/service', {
-					'key' : 'singleModel',
-					'value' : $scope.singleModel + ''
-				});
-			});
-		});
+        $scope.change = function () {
+            var updatedData = {
+                "singleModel": $scope.data.singleModel,
+                "radioModel": $scope.data.radioModel,
+                "checkModel": {
+                    "left": $scope.data.checkModel.left,
+                    "middle": $scope.data.checkModel.middle,
+                    "right": $scope.data.checkModel.right
+                }
+            }
+            console.log("change: " + JSON.stringify(updatedData));
+            $http.post('service', {
+                'key' : 'singleModel',
+                'value' : 'asf'
+            });
+        };
+    });
--- src/main/scala/at/amerbauer/Server.scala
+++ src/main/scala/at/amerbauer/Server.scala
@@ -2,16 +2,19 @@ package at.amerbauer
 
 import akka.actor.ActorSystem
 import akka.util.Timeout
-import spray.routing.SimpleRoutingApp
-import scala.concurrent.duration._
-import java.io.File
-import spray.json.DefaultJsonProtocol
 import spray.httpx.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol
+import spray.routing.SimpleRoutingApp
 
-case class KeyValue(key: String, value: String)
+import scala.concurrent.duration._
+
+case class CheckModel(left: Boolean, middle: Boolean, right: Boolean)
+
+case class Data(singleModel: Int, radioModel: String, checkModel: CheckModel)
 
 object MyJsonProtocol extends DefaultJsonProtocol {
-  implicit val PersonFormat = jsonFormat2(KeyValue)
+  implicit val CheckModelFormat = jsonFormat3(CheckModel)
+  implicit val DataFormat = jsonFormat3(Data)
 }
 
 object Server extends App with SimpleRoutingApp {
@@ -19,21 +22,30 @@ object Server extends App with SimpleRoutingApp {
   implicit val actorSystem = ActorSystem()
   implicit val timeout = Timeout(1.second)
 
-  startServer(interface = "0.0.0.0", port = 8080) {
-    import MyJsonProtocol._
+  // data
+  var data = Data(0, "Middle", CheckModel(true, false, false))
+
+  startServer(interface = "127.0.0.1", port = 8080) {
+    import MyJsonProtocol.DataFormat
     // Let's play web server
     pathPrefix("angularjs12") {
-      getFromDirectory("""src/main/resources/angularjs12""")
+      getFromDirectory( """src/main/resources/angularjs12""")
     } ~
       path("service") {
         post {
-          entity(as[KeyValue]) { keyValue =>
+          entity(as[Data]) { data =>
             complete {
-              println(s"got $keyValue")
+              println(s"got $data")
               "OK"
             }
           }
-        }
+        } ~
+          get {
+            complete {
+              println(s"serving $data")
+              data
+            }
+          }
       }
   }
 }
